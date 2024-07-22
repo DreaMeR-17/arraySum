8af9a04051c6f7f278a794ecf42d213ea58f2e4b
DreaMeR <DreaMeR@DREAMER>
2024-07-22 10:47:13 +0600

    Добавьте файлы проекта.

---
+++ arraySum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace arraySum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;                     
+            int compositionValue = 1;
+            int columnIndex = 0;
+            int lineIndex = 1;
+
+            int[,] array = new int[3, 3]
+            {
+                { 3,5,6 },
+                { 6,7,4 },
+                { 9,1,1 }
+            };
+
+            Console.WriteLine("Дана исходная матрица: ");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                sum += (array[lineIndex, i]);
+            }
+
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                compositionValue *= array[j, columnIndex];
+            }
+
+            Console.WriteLine("\nСумма второй строки: " + sum);
+            Console.WriteLine("\nПроизведение первого столбца: " + compositionValue);
+        }
+    }
+}
---
+++ arraySum/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("arraySum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("arraySum")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("36559c8d-aad6-4ba7-9143-978e7a5f3cf1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
